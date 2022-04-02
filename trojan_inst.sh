8ee2df25fe139ffa1d74457a34d9a3303baead4e
chensh <technology@shuncs.com>
2022-04-02 15:47:45 +0800

    增加bbr

--- trojan_inst.sh
+++ trojan_inst.sh
@@ -270,4 +270,7 @@ config_nginx
 echo "configure trojan-go..."
 config_trojango
 
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
 echo "All is install finish."
