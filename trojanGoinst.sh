0ce3b095e5c5a1b7180df7a0bc6d9f8cd6c41e10
chensh <technology@shuncs.com>
2022-04-02 20:40:52 +0800

    新增trojan的一脚本 原脚本更名trojanGoinst

--- trojanGoinst.sh
+++ trojanGoinst.sh
@@ -2,7 +2,7 @@
 ##################################################
 #
 #
-#功能: 一键安装Tronjan
+#功能: 一键安装Tronjan-go
 #
 #环境: CentOS8
 #
@@ -10,18 +10,18 @@
 #
 #输入 
 #1 证书文件名 
-#2 trojan的版本号 
+#2 Trojan-Go的版本号 
 #3 伪装的网站的文件 
-#4 trojan的密码及域名
+#4 Trojan-Go的密码及域名
 #
 #
 #自动安装Nginx, 并将侦听端口号改为8443。
 #新建 trojan 用户, 使用此用户启动trojan。
-#trojan的默认开启CDN，并侦听443端口。
+#trojan-Go的默认开启CDN，并侦听443端口。
 #
 ##################################################
 
-TrojanPath="/home/trojan"
+TrojanGoPath="/home/trojanGo"
 CertDir="/etc/certs"
 
 toadduser()
@@ -33,10 +33,10 @@ then
     groupadd certuser
 fi
 
-num=`cat /etc/passwd|grep trojan|wc -l`
+num=`cat /etc/passwd|grep trojango|wc -l`
 if [ $num -eq 0 ]
 then
-    useradd -d $TrojanPath -m -G certuser trojan
+    useradd -d $TrojanGoPath -m -G certuser trojango
 fi
 }
 
@@ -51,27 +51,26 @@ systemctl disable nginx
 yum remove nginx -y
 }
 
-uninstall_trojan()
+uninstall_trojango()
 {
-rm -rf $TrojanPath
-rm -rf $filename
+rm -rf $TrojanGoPath"/trojan-go"
+rm -rf trojan-go-linux-amd64.zip
 }
 
-install_trojan()
+install_trojango()
 {
-yum install unzip tar -y
-address="https://github.com/trojan-gfw/trojan/releases/download/v"$version"/"$filename
+yum install unzip -y
+address="https://github.com/p4gefau1t/trojan-go/releases/download/v"$version"/trojan-go-linux-amd64.zip"
 wget $address
 
-if [ ! -f $filename ]
+if [ ! -f trojan-go-linux-amd64.zip ]
 then 
-	echo $filename" is not exist"
+	echo "trojan-go-linux-amd64.zip is not exist"
 	uninstall_nginx
 	exit 1
 fi
 
-tar -xf $filename -C /home
-
+unzip trojan-go-linux-amd64.zip -d $TrojanGoPath
 }
 
 install_nginx()
@@ -126,91 +125,74 @@ rm -rf /usr/share/nginx/html
 unzip $Website -d /usr/share/nginx
 }
 
-config_trojan()
+config_trojango()
 {
 echo "{
     \"run_type\": \"server\",
     \"local_addr\": \"0.0.0.0\",
     \"local_port\": 443,
     \"remote_addr\": \"127.0.0.1\",
-    \"remote_port\": 8443,
+    \"remote_port\": 80,
     \"password\": [
         \""$password"\"
     ],
-    \"log_level\": 1,
     \"ssl\": {
+		\"verify\": true,
+		\"verify_hostname\": true,
+		\"fallback_port\": 8443,
         \"cert\": \""$SSLCert"\",
         \"key\": \""$SSLKey"\",
-        \"key_password\": \"\",
-        \"cipher\": \"ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384\",
-        \"cipher_tls13\": \"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384\",
-        \"prefer_server_cipher\": true,
-        \"alpn\": [
-            \"http/1.1\"
+        \"sni\": \""$Domain"\"
+    },
+    \"router\": {
+        \"enabled\": true,
+        \"block\": [
+            \"geoip:private\"
         ],
-        \"alpn_port_override\": {
-            \"h2\": 81
-        },
-        \"reuse_session\": true,
-        \"session_ticket\": false,
-        \"session_timeout\": 600,
-        \"plain_http_response\": \"\",
-        \"curves\": \"\",
-        \"dhparam\": \"\"
+        \"geoip\": \"$TrojanGoPath"/geoip.dat"\",
+        \"geosite\": \"$TrojanGoPath"/geosite.dat"\"
     },
-    \"tcp\": {
-        \"prefer_ipv4\": false,
-        \"no_delay\": true,
-        \"keep_alive\": true,
-        \"reuse_port\": false,
-        \"fast_open\": true,
-        \"fast_open_qlen\": 20
-    },
-    \"mysql\": {
-        \"enabled\": false,
-        \"server_addr\": \"127.0.0.1\",
-        \"server_port\": 3306,
-        \"database\": \"trojan\",
-        \"username\": \"trojan\",
-        \"password\": \"\",
-        \"cafile\": \"\"
-    }
-}">$TrojanPath/config.json
-cp $TrojanPath/trojan /usr/bin
+ \"websocket\": {
+    \"enabled\": true,
+    \"path\": \"/polarbear\",
+    \"host\": \""$Domain"\"
+  }
+}" >>$TrojanGoPath/server.json
+cp $TrojanGoPath/trojan-go /usr/bin
 
 #生成自启动的服务文件
 echo "[Unit]
-Description=trojan
-Documentation=man:trojan(1) https://trojan-gfw.github.io/trojan/config https://trojan-gfw.github.io/trojan/
-After=network.target network-online.target nss-lookup.target mysql.service mariadb.service mysqld.service
+Description=Trojan-Go - An unidentifiable mechanism that helps you bypass GFW
+Documentation=https://p4gefau1t.github.io/trojan-go/
+After=network.target nss-lookup.target
 
 [Service]
-Type=simple
-StandardError=journal
 User=trojan
-AmbientCapabilities=CAP_NET_BIND_SERVICE
-ExecStart=/usr/bin/trojan -c "$TrojanPath"/config.json
-ExecReload=/bin/kill -HUP $MAINPID
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/trojan-go -config "$TrojanGoPath"/server.json
 Restart=on-failure
-RestartSec=1s
+RestartSec=10s
+LimitNOFILE=infinity
 
 [Install]
 WantedBy=multi-user.target
-">$TrojanPath/trojan.service
+" >$TrojanGoPath/trojan-go.service
 
-cp $TrojanPath/trojan.service /etc/systemd/system
+cp $TrojanGoPath/trojan-go.service /etc/systemd/system
 
-#赋予trojan侦听1024以下的端口
-setcap 'CAP_NET_BIND_SERVICE=+eip' /usr/bin/trojan
+#赋予trojan-go侦听1024以下的端口
+setcap 'CAP_NET_BIND_SERVICE=+eip' /usr/bin/trojan-go
 
 chown -R trojan:certuser $CertDir
-chown -R trojan:certuser $TrojanPath
+chown -R trojan:certuser $TrojanGoPath
 
 chmod -R 750 $CertDir
 
 systemctl daemon-reload
-systemctl enable trojan
-systemctl start trojan
+systemctl enable trojan-go
+systemctl start trojan-go
 }
 
 ################################################################################
@@ -242,17 +224,19 @@ then
 	exit 1
 fi
 
-#trojan版本号
-echo -n "input trojan version:"
+#trojan-go版本号
+echo -n "input trojan-go version:"
 read version
-filename="trojan-"$version"-linux-amd64.tar.xz"
 
 #html
 echo -n "input website zip(include .zip):"
 read Website
 
-echo -n "input trojan password:"
+echo -n "input trojan-go password:"
 read password
+
+echo -n "input domain:"
+read Domain
 
 if [  ! -f $Website ]
 then
@@ -277,14 +261,14 @@ fi
 echo "install nginx......"
 install_nginx
 
-echo "install trojan....."
-install_trojan
+echo "install trojan-go....."
+install_trojango
 
 echo "nginx is install end,configure nginx...."
 config_nginx
 
-echo "configure trojan..."
-config_trojan
+echo "configure trojan-go..."
+config_trojango
 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
