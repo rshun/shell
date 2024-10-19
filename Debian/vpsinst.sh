d709a1f76fe6c716c1b252d2686befac15ba61cc
shun <technology@shuncs.com>
2024-10-19 16:54:08 +0800

    add

--- Debian/vpsinst.sh
+++ Debian/vpsinst.sh
@@ -1,18 +1,27 @@
-apt update
-apt install build-essential man lsof curl wget ufw sqlite3 libsqlite3-dev python3-pip nginx -y
+######################################################################################################################
+#
+# 1. install docker,sqlite3,apps and create user
+# 2. initial user directory
+# 3. enable firewall and open 
+#
+#
+#
+######################################################################################################################
 
-timedatectl set-timezone Asia/Shanghai
+install()
+{
+    apt update
+    apt install build-essential man lsof curl wget ufw sqlite3 libsqlite3-dev python3-pip nginx -y
+}
 
-# adduser and install docker
-wget -O sshd.sh https://raw.githubusercontent.com/rshun/shell/master/Debian/sshd.sh
-wget -O addusr.sh https://raw.githubusercontent.com/rshun/shell/master/Debian/addusr.sh 
-wget -O instdocker.sh https://raw.githubusercontent.com/rshun/shell/master/Debian/instdocker.sh
-chmod +x addusr.sh instdocker.sh sshd.sh
-./sshd.sh
-./instdocker.sh
-./addusr rshun docker
+download()
+{
+    wget -O $shellname https://raw.githubusercontent.com/rshun/shell/master/Debian/$shellname && chmod +x $shellname && ./$shellname $param1
+}
 
-# execute shell
+
+execute_user()
+{
 echo "
 cd /home/rshun
 mkdir -p backup bin csv data etc html lib shell src obj tmp src/stock src/keyMaster src/py src/tmp
@@ -21,9 +30,34 @@ git clone https://github.com/rshun/stock.git
 
 cd /home/rshun/src/keyMaster
 git clone https://github.com/rshun/keyMaster.git
-" >>rshun.sh
+" >>/home/rshun/rshun.sh
 
-chmod 777 rshun.sh
-mv rshun.sh /home/rshun
+chmod 777 /home/rshun/rshun.sh
 su - rshun -c "sh /home/rshun/rshun.sh"
 su - rshun -c "rm /home/rshun/rshun.sh"
+}
+
+enable_firewall()
+{
+    ufw enable
+    ufw allow 39281/tcp
+    ufw reload
+}
+
+#main
+if [ $# -eq 1 ]
+then
+    port = $1
+else
+    port = 39281
+fi
+
+install
+download sshd.sh $port
+download instdocker.sh
+download addusr.sh rshun
+usermod -G docker rshun
+
+timedatectl set-timezone Asia/Shanghai
+execute_user
+enable_firewall
